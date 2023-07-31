cb0eb375fc15c0709fe5f1b0496ac8c91c253598
lucascardemil <lucas.cardemil@hotmail.com>
2023-07-30 22:58:12 -0400

    actualizacion de modulo de axi y axi_objective

--- src/app/components/home/administrator/axi/axi.component.ts
+++ src/app/components/home/administrator/axi/axi.component.ts
@@ -5,10 +5,11 @@ import { NOTYF } from 'src/app/services/notyf/notyf.token';
 import { Notyf } from 'notyf';
 import { FormBuilder, FormControl, FormGroup, Validators } from '@angular/forms';
 import { SubjectService } from 'src/app/services/teacher/subject.service';
-import { Subject } from 'src/app/models/Subject';
 import { AxiService } from 'src/app/services/admin/axi.service';
 import { ResourcesService } from 'src/app/services/resources/resources.service';
 import { PlanningComponent } from '../planning/planning.component';
+import { CourseService } from 'src/app/services/teacher/course.service';
+
 
 @Component({
     selector: 'app-axi',
@@ -18,7 +19,10 @@ import { PlanningComponent } from '../planning/planning.component';
 export class AxiComponent implements OnInit {
 
     list_all_subjects: any = []
+    list_all_update_subjects: any = []
+    list_all_courses: any = []
     list_axis: any = []
+    checkboxs: any = []
 
     pagedItems: any[] = []; // Lista de elementos paginados
 
@@ -26,11 +30,13 @@ export class AxiComponent implements OnInit {
     itemsPerPage: number = 5; // Elementos por página
 
     planningAddAxi = new FormGroup({
+        course: new FormControl(),
         subject: new FormControl(),
         axi: new FormControl(),
     });
 
     planningUpdateAxi = new FormGroup({
+        update_course: new FormControl(),
         update_subject: new FormControl(),
         update_axi: new FormControl(),
     });
@@ -40,6 +46,7 @@ export class AxiComponent implements OnInit {
         public resourcesService: ResourcesService,
         private axiService: AxiService,
         private subjectService: SubjectService,
+        private courseService: CourseService,
         @Inject(NOTYF) private notyf: Notyf,
         private formBuilder: FormBuilder
     ) { }
@@ -47,6 +54,7 @@ export class AxiComponent implements OnInit {
     ngOnInit(): void {
         this.planningAddAxi = this.formBuilder.group(
             {
+                course: ['', [Validators.required]],
                 subject: ['', [Validators.required]],
                 axi: ['', [Validators.required]],
             })
@@ -54,10 +62,12 @@ export class AxiComponent implements OnInit {
         this.planningUpdateAxi = this.formBuilder.group(
             {
                 id: [''],
+                update_course: ['', [Validators.required]],
                 update_subject: ['', [Validators.required]],
                 update_axi: ['', [Validators.required]],
             })
         this.loadAllSubjects()
+        this.loadAllUpdateSubjects()
         this.getAxisForTable()
     }
 
@@ -71,8 +81,22 @@ export class AxiComponent implements OnInit {
     loadAllSubjects() {
         this.list_all_subjects = []
         this.subjectService.getSubject().subscribe((subjects: any) => {
-            subjects.map((subject: Subject) => {
-                this.list_all_subjects.push({
+            const uniqueNames: any = {};
+            this.list_all_subjects = subjects.filter((obj:any) => {
+                if (!uniqueNames[obj.name]) {
+                    uniqueNames[obj.name] = true;
+                    return true;
+                }
+                return false;
+            });
+        })
+    }
+
+    loadAllUpdateSubjects() {
+        this.list_all_update_subjects = []
+        this.subjectService.getSubject().subscribe((subjects: any) => {
+            subjects.map((subject: any) =>{
+                this.list_all_update_subjects.push({
                     id: subject.id,
                     name: subject.name,
                     course: subject.course
@@ -81,20 +105,59 @@ export class AxiComponent implements OnInit {
         })
     }
 
-    savePlanningSubjectAxi(planning: any) {
-        this.axiService.addPlaningSubjectAxi(planning).subscribe((res: any) => {
-            if (res.status === 'success') {
-                this.clearForm();
-                this.notyf.success(res.message);
-                this.getAxisForTable();
-
-                this.axiService.savePlanningSubjectAxi({
-                    id: res.result.id,
-                    name: res.result.subject + '/' + res.result.name
+    searchCourse(event: any) {
+        let id = event.target.value
+        this.list_all_courses = []
+        this.checkboxs = []
+        this.planningAddAxi.patchValue({ course: false });
+        this.courseService.getCourseForSubject(id).subscribe((courses: any) => {
+            courses.map((course: any) => {
+                this.list_all_courses.push({
+                    id: course.id,
+                    name: course.name
                 })
+            })
+        })
+    }
 
-            } else {
-                this.notyf.error(res.message);
+    checkBox(event: any) {
+        let id = parseInt(event.target.id)
+
+        if (event.target.checked === true) {
+            this.checkboxs.push({
+                id: id
+            })
+        } else {
+            this.checkboxs = this.checkboxs.filter((element: any) => element.id !== id)
+        }
+    }
+
+
+
+    savePlanningSubjectAxi(planning: any) {
+        this.checkboxs.map((element: any) => {
+            element.axi = planning.axi,
+            element.subject = planning.subject
+        })
+
+        this.axiService.addPlaningSubjectAxi(this.checkboxs).subscribe((res: any) => {
+            if (res.status === 'success') {
+                const { insertedRecords, existingRecords } = res.result;
+
+                insertedRecords.forEach((record: any) => {
+                    this.notyf.success('¡El Eje ' + record.name + ' y la Asignatura ' + record.subject + ' se asociaron correctamente!');
+                });
+
+                existingRecords.forEach((record: any) => {
+                    this.notyf.error('¡El Eje' + record.name + ' y la Asignatura ' + record.subject + ' ya están asociados!');
+                });
+
+                this.axiService.savePlanningSubjectAxi(insertedRecords)
+
+                this.clearForm();
+                this.getAxisForTable();
+                this.planningAddAxi.patchValue({ course: false });
+                this.checkboxs = []
             }
         });
     }
@@ -106,7 +169,7 @@ export class AxiComponent implements OnInit {
     }
 
     editSelected(axi: any) {
-        const selectedSubject = this.list_all_subjects.find((subject: any) => subject.name === axi.subject);
+        const selectedSubject = this.list_all_update_subjects.find((subject: any) => subject.name === axi.subject && subject.course === axi.course);
         if (selectedSubject) {
             this.planningUpdateAxi.get('update_subject')?.setValue(selectedSubject.id);
         }
@@ -125,7 +188,7 @@ export class AxiComponent implements OnInit {
                     })
 
                     await this.planningComponent.loadPlannings();
-                    
+
                 } else {
                     this.notyf.error(res.message);
                 }
@@ -160,12 +223,20 @@ export class AxiComponent implements OnInit {
         return this.planningAddAxi.get('subject');
     }
 
+    get course() {
+        return this.planningAddAxi.get('course');
+    }
+
     get update_axi() {
         return this.planningUpdateAxi.get('update_axi');
     }
 
     get update_subject() {
         return this.planningUpdateAxi.get('update_subject');
+    }
+
+    get update_course() {
+        return this.planningUpdateAxi.get('update_course');
     }
 
 }
